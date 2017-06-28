796003ff4d9362392bb6c6e2f969d3799d2f5aed
Angel-Avila <angel.armando.96@hotmail.com>
2017-06-28 18:24:44 -0500

    Cleanup

--- work/@m@i@p@s_@processor_@t@b/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity MIPS_Processor_TB is
-end MIPS_Processor_TB;
--- work/@sign@extend/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity SignExtend is
-    port(
-        DataInput       : in     vl_logic_vector(15 downto 0);
-        SignExtendOutput: out    vl_logic_vector(31 downto 0)
-    );
-end SignExtend;
